0eb3f8bce393f70aaab370296357796ef3f7be08
Muzuri Aimable <aimable.muzuri@ircad.africa>
2022-06-15 19:01:04 +0200

    adding check winner in the game

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Query, Req, Res, Session } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request, Response } from 'express';
 
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -13,8 +12,6 @@ export class AppController {
     if(session.id){
       count+=1
     }
-    //console.log(session)
-    //console.log(count);
     session.visits = session.visits ? session.visits + 1 : 1;
     return session.visits;
   }
@@ -23,33 +20,14 @@ export class AppController {
     @Query('board') board: string,
     @Session() session: Record<string, any>
   ) {
-    // if(session.sessionID){
-    //   count+= count
-    // }
-    //console.log(session)
-    // console.log(`this is a session before validateBoard ${session.visits}`);
-   // console.log(`this is a se ${session.visits}`);
+
     const isFirst = session.visits == undefined ? true : false;
-    console.log(`This the first time player ${isFirst}`);
+    console.log(`is it the first time player ${isFirst}`);
     const board1= await this.appService.validateBoard(board, isFirst);
-   // console.log(`this is a session after validateBoard ${session.visits}`);
-    // if(session.visits == undefined){
-    //    console.log('it is the first time request')
-    //    return 'is the first time request'
-    // }
-    await this.appService.processGame(board1);
     session.visits = session.visits ? session.visits + 1 : 1;
-    // if(session.visits ==8){
-    //   session.visits =0;
-    //   return true
-    //  //console.log(session.visits)
-    // }
-   
-    //console.log(session.visits);
-    console.log(`this is a session after processGame ${session.visits}`);
-    // session.visits = 0;
-    // console.log("after setting up session to 0")
-    // console.log(session.visits)
+    const board2 = await this.appService.validateBoard1(board1, session.visits)
+    session.visits = session.visits ? session.visits + 1 : 1;
+    const board3 =  await this.appService.checkWinner(board2);
 
     
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -7,9 +7,7 @@ export class AppService {
   validateBoard(board: string, isFirst: boolean){
 
     const allowedChar = ['x','o',' '];
-    //const board: any[] = Array.from(board);
     console.log(`number of characters in the request ${board.length}`);
-    //  console.log(`this is a se ${session.visits}`);
     if(board.length !==9){
         throw new BadRequestException('Invalid request for board')
     }
@@ -60,87 +58,90 @@ export class AppService {
     return board;
   
   }
-  processGame(board: string){
-    const allowedChar = ['x','o',' '];
-    //const board: any[] = Array.from(board);
-   // console.log(board.length);
-    //let count = 0
-    // if(board.length !==9){
-    //     throw new BadRequestException('Invalid request for board')
-    // }
-    // Array.from(board).map(board => {
-    
-    // if(!(allowedChar.includes(board)))
-    // {
-    //     throw new BadRequestException('Characters should be valid');
-    // }
-    // })
-    // if(count)
-    
-    
-    // this.counter = this.counter+1;
-    // if(!board.includes('x') && !board.includes('o')){
-    //     board[5] === 'o';
+  validateBoard1(board: string, session: number){
 
-    // }
-    
- 
-  
-  
-  //     // // at least 5 moves need to have been made
-  //     if(positions_empty.length <= 4){
-  //       var checkSet = new Set()
-  //       // possible vertical alignments
-  //       if(board[0] && board[3] && board[6] && (Array.from(checkSet.add(board[0]).add(board[3]).add(board)).length === 1)){
-  //         console.log(`${board[3]} Wins!!`);
-  //         //this.endGame();
-  //       }
-  //       checkSet.clear();
-  //       if(board[1] && board[4] && board[7] && (Array.from(checkSet.add(board[1]).add(board[4]).add(board[7])).length === 1)){
-  //         console.log(`${board[4]} Wins!!`);
-  //         //this.endGame();
-  //       }
-  //       checkSet.clear();
-  //       if(board[2] && board[5] && board[8] && (Array.from(checkSet.add(board[2]).add(board[5]).add(board[8])).length === 1)){
-  //         console.log(`${board[2]} Wins!!`);
-  //         //this.endGame();
-  //       }
-  //       checkSet.clear();
-  //       // possible horizontal alignments
-  //       if(board[0] && board[1] && board[2] && (Array.from(checkSet.add(board[0]).add(board[1]).add(board[2])).length === 1)){
-  //         console.log(`${board[1]} Wins!!`);
-  //         //this.endGame();
-  //       }
-  //       checkSet.clear();
-  //       if(board[3] && board[4] && board[5] && (Array.from(checkSet.add(board[3]).add(board[4]).add(board[5])).length === 1)){
-  //         console.log(`${board[3]} Wins!!`);
-  //        // this.endGame();
-  //       }
-  //       checkSet.clear();
-  //       if(board[6] && board[7] && board[8] && (Array.from(checkSet.add(board[6]).add(board[7]).add(board[8])).length === 1)){
-  //         console.log(`${board[8]} Wins!!`);
-  //         //this.endGame(); please clear counter if exist
-  //       }
-  //       checkSet.clear();
-  //       // possible diagonal alignments
-  //       if((board[0] && board[4] && board[8] && (Array.from(checkSet.add(board[0]).add(board[4]).add(board[8])).length === 1)) || (board[2] && board[4] && board[6] && (Array.from(checkSet.add(board[2]).add(board[4]).add(board[6])).length === 1))){
-  //         console.log(`${board[3]} Wins!!`);
-  //         //this.endGame(); this clean counter if exist
-  //       }
-  //       checkSet.clear();
-  //     }
-    
+    var positions_x = [];
+    var positions_o = [];
+    var positions_empty = []; 
+  for (let i=0; i< board.length; i++){
+    if(board[i] === 'o'){
+       positions_o.push(i);
+    }}
+  for (let i=0; i< board.length; i++){
+    if(board[i] === 'x'){
+        positions_x.push(i);
+      }}
+  for (let i=0; i< board.length; i++){
+    if(board[i] === ' '){
+          positions_empty.push(i);
+    }}
+  const occupied_positions = positions_o.concat(positions_x);
+  console.log(`x positions ${positions_x}`);
+  console.log(`o positions ${positions_o}`);
+  console.log(`empty positions ${positions_empty}`);
+  console.log(`occupied positions ${occupied_positions}`);
+  //console.log(isFirst);
+  // check if is the first time player
+  if(session ==1){
+    if(positions_o.length !=1){
+      throw new BadRequestException('Invalid request');
+    }
 
+  }
+    console.log(board);
+    return board;
+  
+  }
+  processGame(session: number, board: string){
+ if(session >= 5){
+  this.checkWinner(board)
+ }
+  }
+  checkWinner(board: string){
+        var checkSet = new Set()
+        // possible vertical alignments
+        if(board[0] && board[3] && board[6] && (Array.from(checkSet.add(board[0]).add(board[3]).add(board)).length === 1)){
+          console.log(`${board[3]} Wins!!`);
+          //this.endGame();
+        }
+        checkSet.clear();
+        if(board[1] && board[4] && board[7] && (Array.from(checkSet.add(board[1]).add(board[4]).add(board[7])).length === 1)){
+          console.log(`${board[4]} Wins!!`);
+          //this.endGame();
+        }
+        checkSet.clear();
+        if(board[2] && board[5] && board[8] && (Array.from(checkSet.add(board[2]).add(board[5]).add(board[8])).length === 1)){
+          console.log(`${board[2]} Wins!!`);
+          //this.endGame();
+        }
+        checkSet.clear();
+        // possible horizontal alignments
+        if(board[0] && board[1] && board[2] && (Array.from(checkSet.add(board[0]).add(board[1]).add(board[2])).length === 1)){
+          console.log(`${board[1]} Wins!!`);
+          //this.endGame();
+        }
+        checkSet.clear();
+        if(board[3] && board[4] && board[5] && (Array.from(checkSet.add(board[3]).add(board[4]).add(board[5])).length === 1)){
+          console.log(`${board[3]} Wins!!`);
+         // this.endGame();
+        }
+        checkSet.clear();
+        if(board[6] && board[7] && board[8] && (Array.from(checkSet.add(board[6]).add(board[7]).add(board[8])).length === 1)){
+          console.log(`${board[8]} Wins!!`);
+          //this.endGame(); please clear counter if exist
+        }
+        checkSet.clear();
+        // possible diagonal alignments
+        if((board[0] && board[4] && board[8] && (Array.from(checkSet.add(board[0]).add(board[4]).add(board[8])).length === 1)) || (board[2] && board[4] && board[6] && (Array.from(checkSet.add(board[2]).add(board[4]).add(board[6])).length === 1))){
+          console.log(`${board[3]} Wins!!`);
+          //this.endGame(); this clean counter if exist
+        }
+        checkSet.clear();
 
     return {
         board_size:board.length,
         board: board.toString().replace(/,/g,''),
         board1: board,
-        // position_x: positions_x,
-        // positions_o: positions_o,
-        // positions_empty: positions_empty,
-        // occupied_positions: occupied_positions
-        // counter: this.counter
     }
   }
   }
